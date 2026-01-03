a7c6836c00b65ba39c9ab6372f1de2cd0cb12151
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-01-03 18:43:40 +0600

    Update Day-19.c

--- Day-19.c
+++ Day-19.c
@@ -168,12 +168,45 @@ printf("%d\n", count);
 
  
 
+
+// Strong number (alada digits gular factoriaal ar jogfol)
+
+int num,sum,temp,i,fact,rem;
+printf("Enter the number : ");
+scanf("%d",&num);
+sum = 0;
+temp = num;
+
+while(temp!=0){
+
+rem = temp%10;
+
+fact=1;
+for(i = 1;i<=rem;i++){
+    fact = fact*i;
+}
+sum = sum + fact;
+temp = temp/10;
+
+}
+
+if(num==sum){
+    printf("Strong Number");
+} 
+else{
+    printf("not a strong number");
+}
+
+
+
+
  
 
  
     return 0;
 
 }
+
 
 
 
